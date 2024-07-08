826bf280aa176e7cb338c4a70ffd7fb75c4da841
priyanshuojha1111 <priyanshuojha111@gmail.com>
2024-07-08 11:59:18 +0530

    resume_update

--- src/component/Home.jsx
+++ src/component/Home.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useRef } from 'react'
-import pdf from '../pdf/Resume.pdf'
+import pdf from '../pdf/Priyanshu_updated_Resume.pdf'
 import hero from '../data/hero.json'
 import Typed from 'typed.js'
 
